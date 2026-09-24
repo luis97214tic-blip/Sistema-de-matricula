841666af13bd1282c293ba7062507bce923053a6
luis97214tic-blip <luis97214tic@gmail.com>
2026-09-23 20:27:38 -0500

    Update paso2_Sin_Retorno.py

---
+++ paso2_Sin_Retorno.py
@@ -0,0 +1,7 @@
+def mostrar_datos(codigo, nombre, apellido, carrera, ciclo, curso):
+    print("Codigo", codigo)
+    print("Nombre:", nombre)
+    print("Apellido:", apellido)
+    print("Carrera:", carrera)
+    print("Ciclo:", ciclo)
+    print("Curso:", curso)
